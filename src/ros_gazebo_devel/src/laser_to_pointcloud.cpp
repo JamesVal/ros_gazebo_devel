56c9371a204c435ae595154e429aa0bd4277b599
james <james.jo.val@gmail.com>
2018-06-27 01:14:56 -0700

    Object avoidance breathing, though it seems to get stuck sometimes.

--- src/ros_gazebo_devel/src/laser_to_pointcloud.cpp
+++ src/ros_gazebo_devel/src/laser_to_pointcloud.cpp
@@ -5,22 +5,26 @@
 #include "laser_geometry/laser_geometry.h"
 
 laser_geometry::LaserProjection projector;
+ros::Publisher cloud_pub_odom;
 ros::Publisher cloud_pub;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   static tf::TransformListener listener;
 
+  sensor_msgs::PointCloud cloud_odom;
   sensor_msgs::PointCloud cloud;
 
   try {
     ros::Time now = ros::Time::now();
     listener.waitForTransform("odom", "laser_sensor", now, ros::Duration(10.0));
-    projector.transformLaserScanToPointCloud("odom",*msg, cloud,listener);
+    projector.transformLaserScanToPointCloud("odom",*msg, cloud_odom,listener);
+    projector.projectLaser(*msg, cloud);
   } catch (tf::TransformException ex) {
     ROS_INFO("L2PC: No transform");
   }
 
+  cloud_pub_odom.publish(cloud_odom);
   cloud_pub.publish(cloud);
 }
 
@@ -29,6 +33,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "laser_to_pointcloud");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("scan_filtered", 1000, laserCallback);
+  cloud_pub_odom = n.advertise<sensor_msgs::PointCloud>("scan_cloud_odom", 1000);
   cloud_pub = n.advertise<sensor_msgs::PointCloud>("scan_cloud", 1000);
   ROS_INFO("Laser To PointCloud running");
   ros::spin();
